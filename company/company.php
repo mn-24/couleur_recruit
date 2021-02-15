af9fb256e83350844fae88ab2c1cf88081f00036
mn-24 <78952830+mn-24@users.noreply.github.com>
2021-02-15 09:47:40 +0900

    Add files via upload

---
+++ company/company.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+ 
+<html lang="ja">
+<link rel="stylesheet" href="company.css">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
+
+<head>
+	<meta charset="utf-8">
+	<title>Company</title>
+</head>
+
+
+<body>
+
+
+<div class="titleArea">
+	<h1>Company</h1>
+</div>
+
+<section>
+	<h2>代表メッセージ</h2>
+	<div class="representativeMessageArea">
+		<div class="representativeMessageBox">
+			<div class="representativeImg">
+				<img src="img/representative.jpg" width="100%" height="100%">
+			</div>
+			<div class="messageTitleArea">
+				<h3>みんなと一緒に<br>
+					チームとして輝ける</h3>
+				<p>株式会社クルール岡崎<br>
+					代表取締役　二木渉子</p>
+			</div>
+		</div>
+		<div class="messageArea">
+			<p>
+				感謝の気持ちを忘れずに、輝きをもち尊厳のある毎日を提供致します。<br>
+				感謝の気持ちを忘れずに、輝きをもち尊厳のある毎日を提供致します。
+				感謝の気持ちを忘れずに、輝きをもち尊厳のある毎日を提供致します。<br>
+				感謝の気持ちを忘れずに、輝きをもち尊厳のある毎日を提供致します。
+				感謝の気持ちを忘れずに、輝きをもち尊厳のある毎日を提供致します。
+				感謝の気持ちを忘れずに、輝きをもち尊厳のある毎日を提供致します。<br>
+				感謝の気持ちを忘れずに、輝きをもち尊厳のある毎日を提供致します。
+			</p>
+		</div>
+	</div>
+</section>
+
+<section>
+	<h2>クルールの理念</h2>
+	<div class="philosophyArea">
+		<div class="philosophyBox">
+			<img src="img/philosophy_1.jpg" width="100%" height="100%">
+			<div class="philosophyFlexBox1">
+				<h3>感謝</h3>
+				<p>感謝の気持ちを忘れずに、輝きをもち<br>
+					尊厳のある毎日を提供致します。</p>
+			</div>
+		</div>
+		<div class="philosophyBox">
+			<img src="img/philosophy_2.jpg" width="100%" height="100%">
+			<div class="philosophyFlexBox2">
+				<h3>感動</h3>
+				<p>満足を超えるサービスに感動できる<br>
+					企業を目指します。</p>
+			</div>
+		</div>
+		<div class="philosophyBox">
+			<img src="img/philosophy_3.jpg" width="100%" height="100%">
+			<div class="philosophyFlexBox3">
+				<h3>絆</h3>
+				<p>助け合う絆でできないことを<br>
+					出来るに変えます。</p>
+			</div>
+		</div>
+		<div class="philosophyBox">
+			<img src="img/philosophy_4.jpg" width="100%" height="100%">
+			<div class="philosophyFlexBox4">
+				<h3>希望</h3>
+				<p>すべてのスタッフで希望の湧く<br>
+				ライフスタイルを目指します。</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section>
+	<h2>クルールのイベント</h2>
+	<div class="eventArea">
+		<div class="eventBox">
+			<img src="img/philosophy_1.jpg" width="100%" height="100%">
+			<div class="eventFlexBox1">
+				<h3>クルール甲子園</h3>
+				<p>施設ならではの魅力をアピール<br>
+					全グループ施設のNo.1が決まります。</p>
+			</div>
+		</div>
+		<div class="eventBox">
+			<img src="img/philosophy_2.jpg" width="100%" height="100%">
+			<div class="eventFlexBox2">
+				<h3>夏祭り</h3>
+				<p>クルール2大イベントの1つ<br>
+					施設ごとにテーマを決めて行います</p>
+			</div>
+		</div>
+		<div class="eventBox">
+			<img src="img/philosophy_3.jpg" width="100%" height="100%">
+			<div class="eventFlexBox3">
+				<h3>クリスマス会</h3>
+				<p>クルール2大イベントの1つ<br>
+					施設ごとにテーマを決めて行います</p>
+			</div>
+		</div>
+		<div class="eventBox">
+			<img src="img/philosophy_4.jpg" width="100%" height="100%">
+			<div class="eventFlexBox4">
+				<h3>年間表彰式</h3>
+				<p>1年を通して最も輝いた<br>
+					スタッフや施設を表彰します</p>
+			</div>
+		</div>
+		<div class="eventBox">
+			<img src="img/philosophy_4.jpg" width="100%" height="100%">
+			<div class="eventFlexBox5">
+				<h3>社員旅行</h3>
+				<p>毎年大好評の社員旅行<br>
+					とっても盛り上がります！</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section>
+	<h2>会社概要</h2>
+	<div class="companyInfArea">
+		<div class="couleurArea">
+			<h3>株式会社クルール岡崎</h3>
+			<p>代表者 : 二木渉子</p>
+			<p>創業 : 令和元年</p>
+			<h4>運営施設</h4>
+			<p>クルール岡崎大和町</p>
+			<p>クルール岡崎矢作町</p>
+			<p>クルール岡崎矢作末広</p>
+		</div>
+		<div class="couleurArea">
+			<h3>株式会社クルール豊田</h3>
+			<p>代表者 : 匹田聖</p>
+			<p>創業 : 令和元年</p>
+			<h4>運営施設</h4>
+			<p>クルール豊田上挙母</p>
+			<p>クルール豊田吉原東館</p>
+			<p>クルール豊田吉原西館</p>
+		</div>
+		<div class="couleurArea">
+			<h3>株式会社クルール尾張</h3>
+			<p>代表者 : 国井真志</p>
+			<p>創業 : 令和元年</p>
+			<h4>運営施設</h4>
+			<p>クルール一宮</p>
+			<p>クルール岐阜長良東</p>
+			<p>クルール津島</p>
+		</div>
+		<div class="couleurArea">
+			<h3>株式会社クルール岐阜</h3>
+			<p>代表者 : 中島弘道</p>
+			<p>創業 : 令和2年</p>
+			<h4>運営施設</h4>
+			<p>クルール各務原</p>
+			<p>クルール大垣</p>
+		</div>
+		<div class="addressArea">
+			<h3>本社所在地</h3>
+			<p>〒460-0002</p>
+			<p>愛知県名古屋市中区丸の内3丁目20-9</p>
+			<p>三晃社ビル 3F</p>
+			<p>TEL : 052-962-0966</p>
+			<p>FAX : 052-962-0967</p>
+		</div>
+	</div>
+</section>
+
+</body>
+</html>
---
+++ recruit/recruit_info.php
@@ -0,0 +1,672 @@
+<!DOCTYPE html>
+ 
+<html lang="ja">
+<link rel="stylesheet" href="recruit_info.css">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
+
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+
+
+<body>
+
+
+<form method="POST">
+	<section>
+		<div class="okazakiArea">
+			<input type="checkbox" id="okazaki" class="couleurArea">
+			<label for="okazaki">クルール岡崎</label>
+			<div class="hiddenBox1">
+				<div class="okazakiAreaBox">
+					<input type="checkbox" id="daiwa" class="couleur">
+					<label for="daiwa">クルール岡崎大和町</label>
+					<div class="hiddenBox2">
+						<div class="staffArea">
+							<ul>
+								<li>
+									<input type="checkbox" id="daiwaCare" class="staff">
+									<label for="daiwaCare">介護福祉士<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="daiwaNightCare" class="staff">
+									<label for="daiwaNightCare">夜勤専従<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="daiwaKitchenStaff" class="staff">
+									<label for="daiwaKitchenStaff">厨房スタッフ<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<input type="checkbox" id="yahagi" class="couleur">
+					<label for="yahagi">クルール岡崎矢作町</label>
+					<div class="hiddenBox2">
+						<div class="staffArea">
+							<ul>
+								<li>
+									<input type="checkbox" id="yahagiCare" class="staff">
+									<label for="yahagiCare">介護福祉士<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="yahagiNightCare" class="staff">
+									<label for="yahagiNightCare">夜勤専従<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="yahagiKitchenStaff" class="staff">
+									<label for="yahagiKitchenStaff">厨房スタッフ<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<input type="checkbox" id="suehiro" class="couleur">
+					<label for="suehiro">クルール岡崎矢作末広</label>
+					<div class="hiddenBox2">
+						<div class="staffArea">
+							<ul>
+								<li>
+									<input type="checkbox" id="suehiroCare" class="staff">
+									<label for="suehiroCare">介護福祉士<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="suehiroNightCare" class="staff">
+									<label for="suehiroNightCare">夜勤専従<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="suehiroKitchenStaff" class="staff">
+									<label for="suehiroKitchenStaff">厨房スタッフ<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="toyotaArea">
+			<input type="checkbox" id="toyota" class="couleurArea">
+			<label for="toyota">クルール豊田</label>
+			<div class="hiddenBox1">
+				<div class="toyotaAreaBox">
+					<input type="checkbox" id="uwagoromo" class="couleur">
+					<label for="uwagoromo">クルール豊田上挙母</label>
+					<div class="hiddenBox2">
+						<div class="staffArea">
+							<ul>
+								<li>
+									<input type="checkbox" id="uwagoromoCare" class="staff">
+									<label for="uwagoromoCare">介護福祉士<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="uwagoromoNightCare" class="staff">
+									<label for="uwagoromoNightCare">夜勤専従<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="uwagoromoKitchenStaff" class="staff">
+									<label for="uwagoromoKitchenStaff">厨房スタッフ<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<input type="checkbox" id="higasikan" class="couleur">
+					<label for="higasikan">クルール豊田吉原東館</label>
+					<div class="hiddenBox2">
+						<div class="staffArea">
+							<ul>
+								<li>
+									<input type="checkbox" id="higasikanCare" class="staff">
+									<label for="higasikanCare">介護福祉士<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="higasikanNightCare" class="staff">
+									<label for="higasikanNightCare">夜勤専従<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="higasikanKitchenStaff" class="staff">
+									<label for="higasikanKitchenStaff">厨房スタッフ<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<input type="checkbox" id="nishikan" class="couleur">
+					<label for="nishikan">クルール豊田吉原西館</label>
+					<div class="hiddenBox2">
+						<div class="staffArea">
+							<ul>
+								<li>
+									<input type="checkbox" id="nishikanCare" class="staff">
+									<label for="nishikanCare">介護福祉士<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="nishikanNightCare" class="staff">
+									<label for="nishikanNightCare">夜勤専従<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="nishikanKitchenStaff" class="staff">
+									<label for="nishikanKitchenStaff">厨房スタッフ<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="owariArea">
+			<input type="checkbox" id="owari" class="couleurArea">
+			<label for="owari">クルール尾張</label>
+			<div class="hiddenBox1">
+				<div class="owariAreaBox">
+					<input type="checkbox" id="ichinomiya" class="couleur">
+					<label for="ichinomiya">クルール一宮</label>
+					<div class="hiddenBox2">
+						<div class="staffArea">
+							<ul>
+								<li>
+									<input type="checkbox" id="ichinomiyaCare" class="staff">
+									<label for="ichinomiyaCare">介護福祉士<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="ichinomiyaNightCare" class="staff">
+									<label for="ichinomiyaNightCare">夜勤専従<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="ichinomiyaKitchenStaff" class="staff">
+									<label for="ichinomiyaKitchenStaff">厨房スタッフ<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<input type="checkbox" id="nagara" class="couleur">
+					<label for="nagara">クルール岐阜長良東</label>
+					<div class="hiddenBox2">
+						<div class="staffArea">
+							<ul>
+								<li>
+									<input type="checkbox" id="nagaraCare" class="staff">
+									<label for="nagaraCare">介護福祉士<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="nagaraNightCare" class="staff">
+									<label for="nagaraNightCare">夜勤専従<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="nagaraKitchenStaff" class="staff">
+									<label for="nagaraKitchenStaff">厨房スタッフ<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<input type="checkbox" id="tsushima" class="couleur">
+					<label for="tsushima">クルール津島</label>
+					<div class="hiddenBox2">
+						<div class="staffArea">
+							<ul>
+								<li>
+									<input type="checkbox" id="tsushimaCare" class="staff">
+									<label for="tsushimaCare">介護福祉士<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="tsushimaNightCare" class="staff">
+									<label for="tsushimaNightCare">夜勤専従<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="tsushimaKitchenStaff" class="staff">
+									<label for="tsushimaKitchenStaff">厨房スタッフ<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="gifuArea">
+		<input type="checkbox" id="gifu" class="couleurArea">
+			<label for="gifu">クルール岐阜</label>
+			<div class="hiddenBox1">
+				<div class="gifuAreaBox">
+					<input type="checkbox" id="kakamigahara" class="couleur">
+					<label for="kakamigahara">クルール各務原</label>
+					<div class="hiddenBox2">
+						<div class="staffArea">
+							<ul>
+								<li>
+									<input type="checkbox" id="kakamigaharaCare" class="staff">
+									<label for="kakamigaharaCare">介護福祉士<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="kakamigaharaNightCare" class="staff">
+									<label for="kakamigaharaNightCare">夜勤専従<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="kakamigaharaKitchenStaff" class="staff">
+									<label for="kakamigaharaKitchenStaff">厨房スタッフ<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<input type="checkbox" id="ogaki" class="couleur">
+					<label for="ogaki">クルール大垣</label>
+					<div class="hiddenBox2">
+						<div class="staffArea">
+							<ul>
+								<li>
+									<input type="checkbox" id="ogakiCare" class="staff">
+									<label for="ogakiCare">介護福祉士<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="ogakiNightCare" class="staff">
+									<label for="ogakiNightCare">夜勤専従<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+								<li>
+									<input type="checkbox" id="ogakiKitchenStaff" class="staff">
+									<label for="ogakiKitchenStaff">厨房スタッフ<span></span></label>
+									<div class="hiddenShow3">
+										<ul>
+											<li>雇用形態</li>
+											<li>給与</li>
+											<li>勤務時間</li>
+											<li>勤務地</li>
+											<li>業務内容</li>
+											<li>必要資格</li>
+											<li>福利厚生</li>
+										</ul>
+										<input type="submit" name="entry" value="ENTRY">
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</form>
+
+
+
+</body>
+</html>
